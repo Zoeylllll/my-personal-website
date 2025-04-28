497b6cfbd4a65dd1d72edbb176b8f205dec2801b
Zoeylllll <2102698390@qq.com>
2025-04-28 14:13:26 +0900

    Fix deployment

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def message():
         return redirect('/message')
     return render_template('message.html', messages=messages)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
